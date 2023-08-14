df3010516aa1075950b93de5cd7ea16849930fa8
pawanprajapat98 <pawanp.rbt@gmail.com>
2023-08-15 01:51:09 +0530

    Ecommerce-using-Bootstrap-and-React

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className='Hero'>
         <div class="card bg-dark text-white border-0">
-              <img src="./NEW.jpg"className="card-img" alt="..." height='635px' width='100%'/>
+              <img src="./new.jpg"className="card-img" alt="..." height='635px' width='100%'/>
                 <div class="card-img-overlay d-flex fd-column   justify-contant-end">
                     <div className='Container'>
                     <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
